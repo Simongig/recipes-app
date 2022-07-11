16a44f5465e66df85c16edae3ecdfc819846de13
Simon Giglhuber <giglhubersi71865@th-nuernberg.de>
2022-07-11 20:06:36 +0200

    updated UI

--- frontend/src/components/RecipeCard-v2.vue
+++ frontend/src/components/RecipeCard-v2.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="{name: 'recipePdp', params: { id: recipe.id }}">
+  <router-link class="card-wrapper" :to="{name: 'recipePdp', params: { id: recipe.id }}">
     <section
       class="card"
       :style="`background-image: url(${recipe.imagePaths[0]})`"
