35788530c3fb3a4989ce5f147223c6f5cad30dc9
Simon Giglhuber <giglhubersi71865@th-nuernberg.de>
2023-08-19 14:52:46 +0200

    whitelabel error fix

--- backend/src/main/java/com/simongig/recipesapp/api/ViewController.java
+++ backend/src/main/java/com/simongig/recipesapp/api/ViewController.java
@@ -1,13 +1,14 @@
 package com.simongig.recipesapp.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/{path:^(?!api).*$}/**")
-    public String forward() {
-        System.out.println("----- FORWARDED -----");
+    @RequestMapping(value = "/{path:^(?!api|css|js)(?!index\\.html$).*}/**")
+    public String forward(@PathVariable String path) {
+        System.out.println("------ Forward - path: " + path);
         return "forward:/";
     }
 }
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,21 +2,14 @@ module.exports = {
     configureWebpack:{
     }, 
     devServer:{
-      host: '0.0.0.0',
-      hot:true,
       // https: true,
       port: 8081,  
       open: 'Chrome',
       proxy: { //https://cli.vuejs.org/guide/html-and-static-assets.html#disable-index-generation
-        '/api/*':{ //everything from root
+        '/*':{ //everything from root
           target: 'http://localhost:8080',
           secure: false,
           ws: false,
-        },
-        '!/*':{ //except root, which is served by webpack's devserver, to faciliate instant updates
-          target: 'http://localhost:8080/',
-          secure: false,
-          ws: false
         },
       }
     }
